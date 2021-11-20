57ecec4522855d558f1f75191a1f5cbad6330bb1
Yosef Groener <yosefgroener@gmail.com>
2021-11-19 19:06:02 -0600

    created sample endpoint requests in index.js

--- index.js
+++ index.js
@@ -2,10 +2,50 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+  console.log(`Listening at listening at http://localhost:${port}`)
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(port, () => {
-  console.log(`Listening at listening at http://localhost:${port}`)
+// Get the questions
+app.get('/qa/questions', (req, res) => {
+  res.send('Here are the questions')
+})
+
+// Get the answers
+app.get('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('Here are the answers')
+})
+
+// Post a question
+app.post('/qa/questions', (req, res) => {
+  res.send('Question was posted')
+})
+
+// Post an answer
+app.post('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('Answer was posted')
+})
+
+// Mark question helpful
+app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  res.send('Question marked as helpful')
+})
+
+// Report Question
+app.put('/qa/questions/:question_id/report', (req, res) => {
+  res.send('Question was reported')
+})
+
+// Mark answer helpful
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  res.send('Answer marked as helpful')
+})
+
+// Report answer
+app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  res.send('Answer marked as helpful')
 })
